8e17c91e5a575161b37ddcc83094782e63aa2a11
Nirupam Pal <nirupampal14@gmail.com>
2026-07-23 10:25:08 +0530

    refactor(repository): format code and improve method structure in BaseRepository

--- backend/src/database/base.repository.ts
+++ backend/src/database/base.repository.ts
@@ -1,10 +1,18 @@
-import { Model } from "mongoose";
+import { Model, ProjectionType, QueryOptions } from 'mongoose';
 
 export class BaseRepository<T extends Document> {
-    constructor(protected readonly model: Model<T>) {}
+  constructor(protected readonly model: Model<T>) {}
 
-    async create(doc: Partial<unknown>): Promise<T> {
-        const createdEntity = new this.model(doc)
-        return createdEntity.save() as Promise<T>;
-    }
+  async create(doc: Partial<unknown>): Promise<T> {
+    const createdEntity = new this.model(doc);
+    return createdEntity.save() as Promise<T>;
+  }
+
+  async findById(
+    id: string,
+    projection?: ProjectionType<T>,
+    options?: QueryOptions,
+  ) {
+    return this.model.findById(id, projection, options).exec();
+  }
 }
