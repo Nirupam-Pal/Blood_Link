507a78320d44a1734539eb2b0d98af100bed0641
Nirupam Pal <nirupampal14@gmail.com>
2026-07-24 14:03:05 +0530

    chore(dependencies): update mongoose and related packages; refactor base repository methods for improved functionality

--- backend/src/database/base.repository.ts
+++ backend/src/database/base.repository.ts
@@ -1,4 +1,11 @@
-import { Model, ProjectionType, QueryOptions } from 'mongoose';
+import {
+  Document,
+  Model,
+  FilterQuery,
+  ProjectionType,
+  QueryOptions,
+  UpdateQuery,
+} from 'mongoose';
 
 export class BaseRepository<T extends Document> {
   constructor(protected readonly model: Model<T>) {}
@@ -12,7 +19,41 @@ export class BaseRepository<T extends Document> {
     id: string,
     projection?: ProjectionType<T>,
     options?: QueryOptions,
-  ) {
+  ): Promise<T | null> {
     return this.model.findById(id, projection, options).exec();
+  }
+
+  async findOne(
+    filter: FilterQuery<T>,
+    projection?: ProjectionType<T>,
+    options?: QueryOptions,
+  ): Promise<T | null> {
+    return this.model.findOne(filter, projection, options).exec();
+  }
+
+  async findMany(
+    filter: FilterQuery<T>,
+    projection?: ProjectionType<T>,
+    options?: QueryOptions,
+  ): Promise<T[] | null> {
+    return this.model.find(filter, projection, options).exec();
+  }
+
+  async update(
+    id: string,
+    update: UpdateQuery<T>,
+    options: QueryOptions = { new: true },
+  ): Promise<T | null> {
+    return this.model.findByIdAndUpdate(id, update, options).exec();
+  }
+
+  async delete(id: string): Promise<T | null> {
+    return this.model
+      .findByIdAndUpdate(id, { isActive: false }, { new: true })
+      .exec();
+  }
+
+  async aggregate(pipeline: any[]): Promise<any[]> {
+    return this.model.aggregate(pipeline).exec();
   }
 }
