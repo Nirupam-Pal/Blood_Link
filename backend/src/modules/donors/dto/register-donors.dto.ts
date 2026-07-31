42ee1e689c0f3b0ecc580cafcccac802f57900a9
Nirupam Pal <nirupampal14@gmail.com>
2026-07-31 15:41:23 +0530

    feat(donors): add additional fields to RegisterDonorDto for comprehensive donor information

---
+++ backend/src/modules/donors/dto/register-donors.dto.ts
@@ -0,0 +1,70 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsNotEmpty, IsNumber, Min } from "class-validator";
+
+export class RegisterDonorDto {
+    @ApiProperty({ example: 70, description: 'Weight in kilograms (Must be >= 45kg)' })
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(1)
+    readonly weight!: number;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly takingMedication!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly pregnantOrBreastFeeding!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly recentTattoo!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly recentSurgery!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly hepatitis!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly hiv!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly diabetes!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly highBloodPressure!: boolean;
+
+    @ApiProperty({ example: false })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly chronicDisease!: boolean;
+
+    @ApiProperty({ example: true })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly consentinformation!: boolean;
+
+    @ApiProperty({ example: true })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly consentContact!: boolean;
+
+    @ApiProperty({ example: true })
+    @IsBoolean()
+    @IsNotEmpty()
+    readonly consentPrivacy!: boolean;
+}
