dbb49ec47c9b60de0ebd7e44e79aea2eee827892
Nirupam Pal <nirupampal14@gmail.com>
2026-08-01 22:54:08 +0530

    feat(blood-banks): implementing registration logic with OTP verification and license check

--- backend/src/modules/blood-banks/blood-banks.service.ts
+++ backend/src/modules/blood-banks/blood-banks.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { BloodBanksRepository } from './repositories/blood-banks.repository';
+import { UsersService } from '../users/users.service';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Otp } from '../auth/schemas/otp.schema';
+import { Connection, Model } from 'mongoose';
+import { RegisterBloodBankDto } from './dto/register-blood-bank.dto';
+import { BloodBank } from './blood-banks.schema';
 
 @Injectable()
-export class BloodBanksService {}
+export class BloodBanksService {
+  constructor(
+    private readonly bloodBanksRepository: BloodBanksRepository,
+    private readonly userService: UsersService,
+    @InjectModel(Otp.name) private readonly otpModel: Model<Otp>,
+    @InjectConnection() private readonly connection: Connection,
+  ) {}
+
+  async register(registerBloodBankDto: RegisterBloodBankDto): Promise<BloodBank> {
+    const verifiedOtp = await this.otpModel.findOne({
+        email: registerBloodBankDto.email.toLowerCase(),
+        isVerified: true,
+    })
+
+    if(!verifiedOtp) {
+        throw new BadRequestException(
+            'Email verification required. Please request and verify an OTP before registering.'
+        );
+    }
+
+    const existingLicense = await this.bloodBanksRepository.findOne({
+        licenseNumber: registerBloodBankDto.licenseNumber
+    });
+
+    if(existingLicense) {
+        throw new ConflictException(
+            'A Blood Bank with this license number is already registered'
+        );
+    }
+
+    const session = await this.connection.startSession();
+    session.startTransaction();
+
+    
+  }
+}
