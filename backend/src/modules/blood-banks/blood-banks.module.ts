4410c6415eee49d6b1cb96289873ce18dd2c4a3b
Nirupam Pal <nirupampal14@gmail.com>
2026-08-02 20:16:24 +0530

    feat(blood-banks): update module to include Mongoose integration for BloodBank and Otp schemas

--- backend/src/modules/blood-banks/blood-banks.module.ts
+++ backend/src/modules/blood-banks/blood-banks.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BloodBanksService } from './blood-banks.service';
 import { BloodBanksController } from './blood-banks.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BloodBank, BloodBankSchema } from './blood-banks.schema';
+import { Otp, OtpSchema } from '../auth/schemas/otp.schema';
+import { UsersModule } from '../users/users.module';
+import { BloodBanksRepository } from './repositories/blood-banks.repository';
 
 @Module({
-  providers: [BloodBanksService],
-  controllers: [BloodBanksController]
+  imports: [
+    MongooseModule.forFeature([
+      { name: BloodBank.name, schema: BloodBankSchema },
+      { name: Otp.name, schema: OtpSchema }
+    ]),
+    UsersModule
+  ],
+  providers: [BloodBanksService, BloodBanksRepository],
+  controllers: [BloodBanksController],
+  exports: [BloodBanksService, BloodBanksRepository]
 })
 export class BloodBanksModule {}
